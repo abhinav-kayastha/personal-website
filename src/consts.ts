22f7220b3222eaac4ede3ced462f9bc1be9087b3
abhin <abhinavk@metropolia.fi>
2026-04-05 13:59:04 +0300

    refactor: update footer and header social links, modify site description, and adjust homepage greeting

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Abhinav Kayastha";
-export const SITE_DESCRIPTION = "Abhinav Kayastha's Personal Website";
+export const SITE_DESCRIPTION = "Abhinav Kayastha's Website";
