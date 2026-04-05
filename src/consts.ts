ce2f944e40ce2a326149e85b254192fb0837ad4f
abhin <abhinavk@metropolia.fi>
2026-04-05 12:39:29 +0300

    initial test commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Abhinav Kayastha";
+export const SITE_DESCRIPTION = "Abhinav Kayastha's Personal Website";
