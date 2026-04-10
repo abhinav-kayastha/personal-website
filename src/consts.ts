bd57b2e26523f06ece6c1d75b86fe2fc5735a334
Abhinav Kayastha <abhinavkayastha04@gmail.com>
2026-04-10 14:08:02 +0300

    many changes

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Abhinav Kayastha";
-export const SITE_DESCRIPTION = "Abhinav Kayastha's Website";
+export const SITE_DESCRIPTION = "RF Software Engineer | Full-Stack Developer | Embedded Systems Specialist";
